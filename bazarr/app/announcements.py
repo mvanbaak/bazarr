0e6fc456d1618f9a2fdbe2223d13112e7b111d4e
morpheus65535 <louis_vezina@hotmail.com>
2026-01-02 20:42:52 -0500

    Added failsafe for announcements in case Statically has issues and we need to fall back to Github.

--- bazarr/app/announcements.py
+++ bazarr/app/announcements.py
@@ -59,14 +59,16 @@ def get_announcements_to_file(job_id=None, startup=False):
             url="https://cdn.statically.io/gh/morpheus65535/bazarr-binaries@refs/heads/master/announcements.json",
             timeout=30
         )
-    except requests.exceptions.HTTPError:
-        logging.exception("Error trying to get announcements from Github. Http error.")
-    except requests.exceptions.ConnectionError:
-        logging.exception("Error trying to get announcements from Github. Connection Error.")
-    except requests.exceptions.Timeout:
-        logging.exception("Error trying to get announcements from Github. Timeout Error.")
-    except requests.exceptions.RequestException:
-        logging.exception("Error trying to get announcements from Github.")
+    except Exception:
+        try:
+            logging.exception("Error trying to get announcements from Statically, falling back to Github.")
+            r = requests.get(
+                url="https://raw.githubusercontent.com/morpheus65535/bazarr-binaries/refs/heads/master/announcements.json",
+                timeout=30
+            )
+        except Exception:
+            logging.exception("Error trying to get announcements from Github.")
+            return
     else:
         with open(os.path.join(args.config_dir, 'config', 'announcements.json'), 'wb') as f:
             f.write(r.content)
